f3feb69f89aa40555e870cc6af593935f49c7cc3
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-08-04 17:06:00 -0300

    Criação do menu

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.ComponentModel;
+
+Console.WriteLine("Escolha a opção desejada:");
+Console.WriteLine("1 - Somar");
+Console.WriteLine("2 - Subtrair");
+Console.WriteLine("3 - Multiplicar");
+Console.WriteLine("4 - Dividir");
+Console.WriteLine("5 - Resto da Divisão");
+Console.WriteLine("6 - Potência");
+Console.WriteLine("0 - SAIR");
+
+int opcao = int.Parse(Console.ReadLine());
