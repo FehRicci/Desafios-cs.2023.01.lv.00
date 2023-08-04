6bc4463f51a202d79dfea42c8f83f43b5d97eec6
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-08-04 17:57:43 -0300

    Calculadora funcionando

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -12,36 +12,35 @@ Console.WriteLine("6 - Potência");
 Console.WriteLine("0 - SAIR");
 
 int opcao = int.Parse(Console.ReadLine());
+
 Console.WriteLine("Informe o primeiro valor:");
 double valor1 = double.Parse(Console.ReadLine());
 Console.WriteLine("Informe o segundo valor:");
 double valor2 = double.Parse(Console.ReadLine());
 
-do
-{
-    switch (opcao)
-    {
-        case 1:
-            Console.WriteLine("O Resultado é: " + Operacoes.Somar(valor1, valor2));
-            break;
-        case 2:
-            Console.WriteLine("O Resultado é: " + Operacoes.Subtrair(valor1, valor2));
-            break;
-        case 3:
-            Console.WriteLine("O Resultado é: " + Operacoes.Multiplicar(valor1, valor2));
-            break;
-        case 4:
-            Console.WriteLine("O Resultado é: " + Operacoes.Dividir(valor1, valor2));
-            break;
-        case 5:
-            Console.WriteLine("O Resultado é: " + Operacoes.Resto(valor1, valor2));
-            break;
-        case 6:
-            Console.WriteLine("O Resultado é: " + Operacoes.Potencia(valor1, valor2));
-            break;
-        default:
-            break;
 
-    }
-}while(opcao != 0);
+switch (opcao)
+{
+    case 1:
+        Console.WriteLine("O Resultado é: " + Operacoes.Somar(valor1, valor2));
+        break;
+    case 2:
+        Console.WriteLine("O Resultado é: " + Operacoes.Subtrair(valor1, valor2));
+        break;
+    case 3:
+        Console.WriteLine("O Resultado é: " + Operacoes.Multiplicar(valor1, valor2));
+        break;
+    case 4:
+        Console.WriteLine("O Resultado é: " + Operacoes.Dividir(valor1, valor2));
+        break;
+    case 5:
+        Console.WriteLine("O Resultado é: " + Operacoes.Resto(valor1, valor2));
+        break;
+    case 6:
+        Console.WriteLine("O Resultado é: " + Operacoes.Potencia(valor1, valor2));
+        break;
+    default:
+        break;
+
+}
 
