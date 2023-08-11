bd14cdbe6445447d96d9b9c7ed24ab652acee4b7
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-08-11 13:02:28 -0300

    Criando Menu

--- Dados/Dados/Program.cs
+++ Dados/Dados/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Bom vindo ao jogo de dados!");
+Console.WriteLine("Informe o nome do Primeiro Jogador: ");
+var nome1 = Console.ReadLine();
+Console.WriteLine("Informe o nome do Segundo Jogador: ");
+var nome2 = Console.ReadLine();
