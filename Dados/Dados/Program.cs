9d1f2207ddb08201a8f0596fdb153ca748910e97
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2023-08-11 13:11:40 -0300

    Criada a condição para o menu

--- Dados/Dados/Program.cs
+++ Dados/Dados/Program.cs
@@ -1,6 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Bom vindo ao jogo de dados!");
-Console.WriteLine("Informe o nome do Primeiro Jogador: ");
-var nome1 = Console.ReadLine();
-Console.WriteLine("Informe o nome do Segundo Jogador: ");
-var nome2 = Console.ReadLine();
+Console.WriteLine("Bom vindo ao jogo de dados!\n");
+string nome1;
+string nome2;
+
+do
+{
+    Console.WriteLine("Informe o nome do Primeiro Jogador: ");
+    nome1 = Console.ReadLine().ToUpper();
+
+    Console.WriteLine("Informe o nome do Segundo Jogador: ");
+    nome2 = Console.ReadLine().ToUpper();
+
+    if (nome1 == nome2)
+    {
+        Console.WriteLine("\nOs nomes não podem ser iguais!\n");
+    }
+}while (nome1 == nome2);
